3fc4f313cb6a1dd20cd7709be0c609f329ddb6f0
Freddy Carmona Hernandez <soyfreddych@gmail.com>
2024-06-21 22:04:34 -0700

    Javascript and css linked to html

---
+++ main.js
@@ -0,0 +1,16 @@
+// Assignment
+
+/* 
+
+Haz una pagina que tiene un buton
+color negro, con texto color azul
+centrado verticalmente y
+horizontalmente
+
+*/
+
+function testCode() {
+    console.log("linked successfully")
+}
+
+testCode()
