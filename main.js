94f15c1083d887048d074b7b2ee0da952bb1ffa1
Freddy Carmona Hernandez <soyfreddych@gmail.com>
2024-06-24 12:33:51 -0700

    button can change background color red and turn it back to green

--- main.js
+++ main.js
@@ -2,15 +2,33 @@
 
 /* 
 
-Haz una pagina que tiene un buton
-color negro, con texto color azul
-centrado verticalmente y
-horizontalmente
+Cuando haces clic en el buton, que cambia
+el color del fondo de pagina,
+de verde a morado
+
+event-listener
+
+necesitas anadir un event-listener, 
+al buton
 
 */
 
-function testCode() {
-    console.log("linked successfully")
+function changeBackgroundColor() {
+  const bodyElement = document.querySelector("body");
+  const currentBackgroundColor = bodyElement.style.backgroundColor;
+
+  //console.log(currentBackgroundColor);
+
+  if (currentBackgroundColor === "red") {
+    bodyElement.style.backgroundColor = "rgb(135, 168, 130)";
+  } else {
+    bodyElement.style.backgroundColor = "red";
+  }
 }
 
-testCode()
+function addButtonEventListener() {
+  const buttonElement = document.querySelector("#background-changer");
+  buttonElement.addEventListener("click", changeBackgroundColor);
+}
+
+addButtonEventListener();
